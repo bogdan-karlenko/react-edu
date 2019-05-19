bd01cb61935cf2b658fab04c9733163373882157
Bogdan Karlenko <opposite.ps@gmail.com>
2019-05-19 23:29:16 +0300

    initial commit

---
+++ build/precache-manifest.64cbec8db74ac25c7deb1ac504c64f11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0378b0f4c7515080397f3a4df190d1bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6910f67b0020b9f73581",
+    "url": "/static/css/main.06ac80c3.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "6910f67b0020b9f73581",
+    "url": "/static/js/main.3d75599b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
